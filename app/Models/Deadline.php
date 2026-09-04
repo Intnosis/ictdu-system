0d9948e10f38c7e8d4b0d0125e8f165f46cdea1f
Ma. Cristina V. Sison <macristinasison55@gmail.com>
2026-09-05 07:20:11 +0800

    Model deadline.php

--- app/Models/Deadline.php
+++ app/Models/Deadline.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Deadline extends Model
 {
-    //
+    protected $fillable = [
+    'title'
+    'description'
+    'deadline'
+    'user_id'
+    ];
+    
+    public function user ()
+    {
+        return $this->belongsTo(User::Class);
 }
