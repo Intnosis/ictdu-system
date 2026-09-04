9c3e692ceca784b6c07a7b57579d1b0180900262
Ma. Cristina V. Sison <macristinasison55@gmail.com>
2026-09-05 07:53:28 +0800

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,4 +2,38 @@
 
 namespace App\Http\Controller;
 
-use App\Model
+use App\Model\User;
+use App\Model\Announcement;
+use App\Model\GitHubLink;
+use App\Model\Report;
+use App\Model\Event;
+use App\Model\Deadline;
+
+class DashboardController extends Controller{
+
+    public function index()
+    {
+        return reponse()->json([
+        'total_users' => User::count(),
+        'total_announcements' => Announcement::count(),
+        'total_deadlines' => Deadline::count(),
+        'total_github_projects' => GitHubLink::count(),
+        'total_reports' => Report::count(),
+        'total_events' => Event::count(),
+        'total_deadlines' => Deadline::count(),
+        
+        'recent_announcements' => Announcement::latest()
+            ->take(5)->get(),
+            
+        'upcomming_deadlines' => Deadline::where('deadline', '>=',
+            now()->toDateString()
+            )
+            ->orderBy('deadline')
+            ->take(5)
+            ->get()
+            
+            ]);
+            
+        }
+
+}
