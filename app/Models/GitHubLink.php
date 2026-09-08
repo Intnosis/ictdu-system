95c26e5e53ca63298259b49b2a00b1af456ae3ea
Ma. Cristina V. Sison <macristinasison55@gmail.com>
2026-09-08 18:39:12 +0800

    Update Model.php

--- app/Models/GitHubLink.php
+++ app/Models/GitHubLink.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class GitHubLink extends Model
 {
-    //
+    protected $fillable = [ 'project_name', 'repo_link', 'description', 'tech_stack', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
