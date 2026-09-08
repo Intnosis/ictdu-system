db4045047fd785a98268e2c703fe70fab9bdef17
Ma. Cristina V. Sison <macristinasison55@gmail.com>
2026-09-08 18:43:36 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function gitHubLinks()
+    {
+        return $this->hasMany(GitHubLink::class);
+    }
 }
