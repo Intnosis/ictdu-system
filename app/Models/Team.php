3437b6380a89031d975cbc4f520999081ee91edf
Ma. Cristina V. Sison <macristinasison55@gmail.com>
2026-09-22 16:36:43 +0800

    update team_models

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -6,10 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    
+    protected $fillable = [
+        'team_name',
+        'deacription'
+    ];
 
     public function team_members()
     {
         return $this->hasMany(Team_Members::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
