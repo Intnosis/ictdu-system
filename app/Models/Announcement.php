a91e7f12d76d7a03f4247fed684438745432edcb
Ma. Cristina V. Sison <macristinasison55@gmail.com>
2026-09-23 15:12:48 +0800

    Update Announcement.php

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -10,6 +10,10 @@ class Announcement extends Model
        'tile',
        'content'
     ];
+
+    protected $hidden = [
+      'timetamps'
+    ];
     public function user()
     {
        return $this->belongsTo(User::class);
