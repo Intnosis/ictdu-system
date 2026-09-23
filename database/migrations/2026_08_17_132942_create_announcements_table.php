25115fc2738f9eb9af7802635d82a1c5c2b21a5e
Ma. Cristina V. Sison <macristinasison55@gmail.com>
2026-09-23 16:38:48 +0800

    debugging_updates

--- database/migrations/2026_08_17_132942_create_announcements_table.php
+++ database/migrations/2026_08_17_132942_create_announcements_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('announcements', function (Blueprint $table) {
             $table->id();
-            $table->string('tile');
+            $table->string('title');
             $table->string('content');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
